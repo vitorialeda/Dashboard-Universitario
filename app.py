475f113ba0a341dc264a08c2adba6bb6385f3663
Vitoria Leda <vitoria_lnogueira@hotmail.com>
2025-02-16 19:19:34 -0300

    criação do bd e da passta template

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request # type: ignore
+import sqlite3
 
 app = Flask(__name__)
+app.run(debug=True)
+
+con = sqlite3.connect("dashboard.db")
+cur = con.cursor()
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Helloooo</p>"
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
