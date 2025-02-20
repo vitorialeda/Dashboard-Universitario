d96b9350ee14bb00e0ef6898d3c71221695e6d1a
Vitoria Leda <vitoria_lnogueira@hotmail.com>
2025-02-20 14:27:11 -0300

    testando db, adiciona pasta templates contendo arquivo html

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ import sqlite3
 app = Flask(__name__)
 app.run(debug=True)
 
-con = sqlite3.connect("dashboard.db")
+con = sqlite3.connect("dashboard.db", check_same_thread=False)
+def dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
+con.row_factory = dict_factory
 cur = con.cursor()
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Helloooo</p>"
-
-
-@app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    # resposta = cur.execute('SELECT * FROM disciplina')
+    # disciplina = resposta.fetchone()
+    # print(disciplina[0])
+    cur.execute("SELECT * FROM disciplina")
+    disciplina = cur.fetchall()
+    print(disciplina)
+    return render_template("dashboard.html", disciplina = disciplina)
