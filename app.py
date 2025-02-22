ee5492a1b7892fb9e976db78902069d770dee8df
Vitoria Leda <vitoria_lnogueira@hotmail.com>
2025-02-22 16:21:46 -0300

    funcionalidades proximos conteudos e proximas atividades

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request # type: ignore
 import database
 
 app = Flask(__name__)
-app.run(debug=True)
-
-
 
 
 @app.route("/")
 def dashboard():
     disciplinas = database.mostra_disciplinas()
     conteudos = database.mostra_conteudos()
-    return render_template("dashboard.html", disciplinas = disciplinas, conteudos = conteudos, tamanho = len(disciplinas))
+    atividades = database.mostra_atividades()
+    proximos_conteudos = database.proximos_conteudos()
+    proximas_atividades = database.proximas_atividades()
+    
+    return render_template("dashboard.html", disciplinas = disciplinas, conteudos = conteudos, tamanho = len(disciplinas), atividades = atividades, proximos_conteudos=proximos_conteudos, proximas_atividades=proximas_atividades)
--- database.py
+++ database.py
@@ -22,8 +22,44 @@ def mostra_conteudos():
     connect.row_factory = dict_factory
     cursor = connect.cursor()
 
-    cursor.execute("SELECT disciplina_id_disciplina, titulo FROM conteudo ORDER BY disciplina_id_disciplina")
+    cursor.execute("SELECT disciplina_id_disciplina AS disciplina, titulo FROM conteudo ORDER BY disciplina_id_disciplina")
     conteudos = cursor.fetchall()
 
     connect.close()
     return conteudos
+
+def mostra_atividades():
+    connect = sqlite3.connect("dashboard.db")
+    connect.row_factory = dict_factory
+    cursor = connect.cursor()
+
+    cursor.execute("SELECT titulo, descricao, status, disciplina_id_disciplina as disciplina, prazo FROM atividade")
+    atividades = cursor.fetchall()
+
+    connect.close()
+    return atividades
+
+def proximos_conteudos():
+    connect = sqlite3.connect("dashboard.db")
+    connect.row_factory = dict_factory
+    cursor = connect.cursor()
+
+    cursor.execute("SELECT titulo, disciplina_id_disciplina AS disciplina FROM conteudo GROUP BY disciplina")
+    conteudos = cursor.fetchall()
+
+    connect.close()
+    return conteudos
+
+def proximas_atividades():
+    connect = sqlite3.connect("dashboard.db")
+    connect.row_factory = dict_factory
+    cursor = connect.cursor()
+
+    cursor.execute("SELECT * FROM atividade ORDER BY prazo")
+    atividades = cursor.fetchall()
+
+    # TO DO
+    # Fazer com que a query puxe atividades da proxima semana
+
+    connect.close()
+    return atividades
