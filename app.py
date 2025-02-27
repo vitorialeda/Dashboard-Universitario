14c30eefb10f8f403e0222b5d1af4a92281e9899
Vitoria Leda <vitoria_lnogueira@hotmail.com>
2025-02-27 18:37:38 -0300

    implementação progresso, imagem de apoio

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ def dashboard():
     prox_conteudos = database.proximos_conteudos()
     prox_atividades = database.proximas_atividades()
     disciplinas = database.mostra_disciplinas()
+    progresso = database.progresso()
 
     for i in range(len(disciplinas)):
         disciplinas[i]["ementa"] = database.mostra_conteudos(disciplinas[i]["id"])
         disciplinas[i]["atividades"] = database.mostra_atividades(disciplinas[i]["id"])
  
 
-    return render_template("index.html", disciplinas= disciplinas, prox_conteudos=prox_conteudos, prox_atividades=prox_atividades)
+    return render_template("index.html", disciplinas= disciplinas, prox_conteudos=prox_conteudos, prox_atividades=prox_atividades, progresso = progresso)
 
 
 @app.route("/atualizaStatus", methods=['POST'])
--- database.py
+++ database.py
@@ -6,66 +6,93 @@ def dict_factory(cursor, row):
     return {key: value for key, value in zip(fields, row)}
 
 
-# Puxa da base de dados o nome e o objetivo de uma disciplina
 def mostra_disciplinas():
+    # Contecta à base de dados
     connect = sqlite3.connect("dashboard.db")
     connect.row_factory = dict_factory
     cursor = connect.cursor()
 
+    # Puxa da base de dados o nome e o objetivo de uma disciplina
     cursor.execute("SELECT id_disciplina AS id, nome, objetivo FROM disciplina")
+    
+    # Guarda resultado da Query na variavel
     disciplinas = cursor.fetchall()
 
+    # Fecha conexão com o banco de dados 
     connect.close()
     return disciplinas
 
 
-# Puxa da base de dados o titulo, o status e a disciplina dos conteudos de uma disciplina
 def mostra_conteudos(id_disciplina):
     connect = sqlite3.connect("dashboard.db")
     connect.row_factory = dict_factory
     cursor = connect.cursor()
 
-    cursor.execute("SELECT id, nome, concluido FROM todo WHERE categoria = 'CON' AND id_disciplina = ?", (id_disciplina,))
+    # Puxa da base de dados o titulo, o status e a disciplina dos conteudos de uma disciplina
+    cursor.execute(
+        '''
+        SELECT id, nome, concluido FROM todo 
+        WHERE categoria = 'CON' AND id_disciplina = ?
+        ''', (id_disciplina,))
+    
     conteudos = cursor.fetchall()
 
     connect.close()
     return conteudos
 
 
-# Puxa da base de dados o titulo, o status e a disciplina das atividades de uma disciplina
 def mostra_atividades(id_disciplina):
     connect = sqlite3.connect("dashboard.db")
     connect.row_factory = dict_factory
     cursor = connect.cursor()
 
-    cursor.execute("SELECT id, nome, concluido, prazo, id_disciplina as disciplina FROM todo WHERE categoria = 'ATV' AND disciplina = ?", (id_disciplina,))
+    # Puxa da base de dados o titulo, o status e a disciplina das atividades de uma disciplina
+    cursor.execute(
+        '''
+        SELECT id, nome, concluido, prazo, id_disciplina as disciplina FROM todo 
+        WHERE categoria = 'ATV' AND disciplina = ?
+        ''', (id_disciplina,))
+    
     atividades = cursor.fetchall()
 
     connect.close()
     return atividades
 
 
-# Puxa o proximo conteudo a fazer de cada disciplina
 def proximos_conteudos():
     connect = sqlite3.connect("dashboard.db")
     connect.row_factory = dict_factory
     cursor = connect.cursor()
 
-    cursor.execute("SELECT nome, concluido, id_disciplina AS disciplina FROM todo WHERE categoria = 'CON' AND concluido = 'False' GROUP BY id_disciplina")
+    # Puxa o proximo conteudo a fazer de cada disciplina
+    cursor.execute(
+        '''
+        SELECT nome, concluido, id_disciplina AS disciplina FROM todo 
+        WHERE categoria = 'CON' AND concluido = 'False' 
+        GROUP BY id_disciplina
+        ''')
+    
     prox_conteudos = cursor.fetchall()
 
     connect.close()
     return prox_conteudos
 
 
-# Puxa as atividades com o prazo de entrega para os proximos 7 dias
 def proximas_atividades():
     connect = sqlite3.connect("dashboard.db")
     connect.row_factory = dict_factory
     cursor = connect.cursor()
-
+    
+    # Guarda na variavel o dia de hoje na ordem ano mes e dia
     hoje = datetime.today().strftime('%Y-%m-%d')
-    cursor.execute("SELECT nome, prazo, id_disciplina AS disciplina FROM todo WHERE julianday(prazo) - julianday(?) <= 7", (hoje,))
+
+    # Puxa as atividades com o prazo de entrega para os proximos 7 dias
+    cursor.execute(
+        '''
+        SELECT nome, prazo, id_disciplina AS disciplina FROM todo 
+        WHERE julianday(prazo) - julianday(?) <= 7
+        ''', (hoje,))
+    
     prox_atividades = cursor.fetchall()
 
     connect.close()
@@ -76,19 +103,62 @@ def atualizaStatus(id, status):
     connect = sqlite3.connect("dashboard.db")
     cursor = connect.cursor()
 
+    # Modifica o status da atividade/conteudo
     cursor.execute("UPDATE todo SET concluido = ? WHERE id = ?", (status, id) )
     
+    # Commit do update no banco de dados
     connect.commit()
+
     connect.close()
     return
+
 
 def mostra_eventos():
     connect = sqlite3.connect("dashboard.db")
     connect.row_factory = dict_factory
     cursor = connect.cursor()
 
-    cursor.execute("SELECT id, nome, prazo, id_disciplina AS disciplina, concluido FROM todo WHERE prazo NOT NULL")
+    cursor.execute(
+        '''
+        SELECT id, nome, prazo, id_disciplina AS disciplina, concluido FROM todo 
+        WHERE prazo NOT NULL
+        ''')
+    
     eventos = cursor.fetchall()
 
     connect.close()
     return eventos
+
+def progresso():
+    # Guarda numero do mes atual na variavel
+    mesAtual = datetime.today().strftime("%m")
+
+    connect = sqlite3.connect("dashboard.db")
+    connect.row_factory = dict_factory
+    cursor = connect.cursor()
+
+
+### MUDAR 03 POR ? , (mesAtual,)
+
+    # Verifica a quantidade de tarefas a serem entregues no mes atual
+    cursor.execute(
+        '''
+        SELECT COUNT(*) AS total FROM todo 
+        WHERE strftime('%m', prazo) = '03'
+        ''')
+    
+    # , (mesAtual,)
+    
+    totalAtividadesMes = cursor.fetchall()
+
+    # Verifica quantas tarefas foram feitas 
+    cursor.execute(
+        '''
+        SELECT COUNT(*) AS concluidas FROM todo
+        WHERE strftime('%m', prazo) = '03'
+        AND concluido = 'True'
+        ''')
+    
+    atvConcluidasMes = cursor.fetchall()
+    
+    return {"total": totalAtividadesMes[0]["total"], "concluidas": atvConcluidasMes[0]["concluidas"]}
